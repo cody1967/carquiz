52e85fae7e1f98a37902c306ea9994d4c1ee24df
Cody McNeill <cody@utahhydrographics.com>
2021-02-22 19:57:33 -0700

    questions.js

---
+++ assets/questions.js
@@ -0,0 +1,42 @@
+
+
+
+var questions = [
+  {
+      title: "What Color Of Car Hides The Dirt The Best?",
+      choices: ["White", "Blue", "black", "red"],
+      answer: "white"
+  },
+
+  {
+      title: "What Car Is A Diesel",
+      choices: ["Vw Passat TdI", "BMW 330 ci", "Ford Focus", "Toyota Camry"],
+      answer: "Vw Passat TDI"
+  },
+
+  {
+      title: "What Car Has The Fastest Top Speed @ 261MPH",
+      choices: ["2020 Aston Martin DBS Superleggera ", "2020 Ferrari F8", "2020 Bugutti Chiron ", "2020 Ford GT"],
+      answer: "2020 Bugutti Chiron"
+  },
+
+  {
+      title: "This Is A List Of Energy Systems That Cars Use Whats The Least Common? ",
+      choices: ["Gasoline", "Hydrogen Fuel Cell", "Diesel", "Electric"],
+      answer: "Hydrogen Fuel Cell"
+  },
+
+  {
+      title: "What Car is AWD",
+      choices: ["Subaru Outback", "Chevy Camero", "Ford Mustang", "Dodge Viper"],
+      answer: "Subaru Outback"
+  },
+
+  {
+      title: "What Car Has A SMG Transmission",
+      choices: ["BMW E46 M3", "Ford Mustang", "Chevy Cruze", "Chevy Malibu"],
+      answer: "BMW E46 M3"
+  },
+
+
+];
